ca3ac9738073c92ed0ef46980f786b1e40836700
Manikantan <dj.kantan11@gmail.com>
2019-03-14 07:37:24 -0400

    Add files via upload
    XML reader version 1.0.1

---
+++ xmlreader/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace xmlreader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.BtnLoad = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.BtnSearch = new System.Windows.Forms.Button();
+            this.dataSet1 = new System.Data.DataSet();
+            this.CMBsearchcol = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnReset = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BTNABOUT = new System.Windows.Forms.Button();
+            this.BTnMax = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonShadow;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Sunken;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(878, 262);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // BtnLoad
+            // 
+            this.BtnLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnLoad.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(209)))), ((int)(((byte)(181)))));
+            this.BtnLoad.Location = new System.Drawing.Point(618, 12);
+            this.BtnLoad.Name = "BtnLoad";
+            this.BtnLoad.Size = new System.Drawing.Size(75, 23);
+            this.BtnLoad.TabIndex = 1;
+            this.BtnLoad.Text = "Load File";
+            this.BtnLoad.UseVisualStyleBackColor = true;
+            this.BtnLoad.Click += new System.EventHandler(this.BtnLoad_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(130, 37);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(192, 19);
+            this.txtSearch.TabIndex = 2;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // BtnSearch
+            // 
+            this.BtnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnSearch.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(209)))), ((int)(((byte)(181)))));
+            this.BtnSearch.Location = new System.Drawing.Point(328, 8);
+            this.BtnSearch.Name = "BtnSearch";
+            this.BtnSearch.Size = new System.Drawing.Size(75, 23);
+            this.BtnSearch.TabIndex = 3;
+            this.BtnSearch.Text = "Search";
+            this.BtnSearch.UseVisualStyleBackColor = true;
+            this.BtnSearch.Click += new System.EventHandler(this.BtnSearch_Click);
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // CMBsearchcol
+            // 
+            this.CMBsearchcol.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CMBsearchcol.FormattingEnabled = true;
+            this.CMBsearchcol.Items.AddRange(new object[] {
+            "<--------SELECT---------->"});
+            this.CMBsearchcol.Location = new System.Drawing.Point(130, 10);
+            this.CMBsearchcol.Name = "CMBsearchcol";
+            this.CMBsearchcol.Size = new System.Drawing.Size(192, 21);
+            this.CMBsearchcol.TabIndex = 4;
+            this.CMBsearchcol.SelectedIndexChanged += new System.EventHandler(this.CMBsearchcol_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(209)))), ((int)(((byte)(181)))));
+            this.label1.Location = new System.Drawing.Point(5, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Column To Search :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(209)))), ((int)(((byte)(181)))));
+            this.label2.Location = new System.Drawing.Point(13, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Value To Search :";
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnReset.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(209)))), ((int)(((byte)(181)))));
+            this.BtnReset.Location = new System.Drawing.Point(328, 33);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(75, 23);
+            this.BtnReset.TabIndex = 7;
+            this.BtnReset.Text = "Reset";
+            this.BtnReset.UseVisualStyleBackColor = true;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(86)))), ((int)(((byte)(142)))), ((int)(((byte)(166)))));
+            this.panel1.Controls.Add(this.BTNABOUT);
+            this.panel1.Controls.Add(this.BTnMax);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.lblHeader);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(878, 27);
+            this.panel1.TabIndex = 8;
+            // 
+            // BTNABOUT
+            // 
+            this.BTNABOUT.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BTNABOUT.Dock = System.Windows.Forms.DockStyle.Right;
+            this.BTNABOUT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTNABOUT.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(209)))), ((int)(((byte)(181)))));
+            this.BTNABOUT.Location = new System.Drawing.Point(789, 0);
+            this.BTNABOUT.Name = "BTNABOUT";
+            this.BTNABOUT.Size = new System.Drawing.Size(27, 27);
+            this.BTNABOUT.TabIndex = 3;
+            this.BTNABOUT.Text = "?";
+            this.BTNABOUT.UseVisualStyleBackColor = true;
+            this.BTNABOUT.Click += new System.EventHandler(this.BTNABOUT_Click);
+            // 
+            // BTnMax
+            // 
+            this.BTnMax.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BTnMax.Dock = System.Windows.Forms.DockStyle.Right;
+            this.BTnMax.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTnMax.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(209)))), ((int)(((byte)(181)))));
+            this.BTnMax.Location = new System.Drawing.Point(816, 0);
+            this.BTnMax.Name = "BTnMax";
+            this.BTnMax.Size = new System.Drawing.Size(27, 27);
+            this.BTnMax.TabIndex = 2;
+            this.BTnMax.UseVisualStyleBackColor = true;
+            this.BTnMax.Click += new System.EventHandler(this.BTnMax_Click);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(209)))), ((int)(((byte)(181)))));
+            this.button1.Location = new System.Drawing.Point(843, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lblHeader.Font = new System.Drawing.Font("Stencil", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(209)))), ((int)(((byte)(181)))));
+            this.lblHeader.Location = new System.Drawing.Point(0, 0);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(124, 22);
+            this.lblHeader.TabIndex = 0;
+            this.lblHeader.Text = "XML READER";
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.BtnSearch);
+            this.panel2.Controls.Add(this.BtnLoad);
+            this.panel2.Controls.Add(this.BtnReset);
+            this.panel2.Controls.Add(this.txtSearch);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.CMBsearchcol);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(0, 293);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(878, 97);
+            this.panel2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(95)))), ((int)(((byte)(114)))));
+            this.ClientSize = new System.Drawing.Size(878, 390);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "XML Reader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button BtnLoad;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button BtnSearch;
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.ComboBox CMBsearchcol;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnReset;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BTnMax;
+        private System.Windows.Forms.Button BTNABOUT;
+    }
+}
+
---
+++ xmlreader/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace xmlreader
+{
+
+    public partial class Form1 : Form
+    {
+        string searchItem;
+        string path;
+        int rst = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+            DataSet dataSet = new DataSet();
+            CMBsearchcol.Items.Clear();     
+            path = "";
+            openFileDialog1.Filter = "Xml Files|*.xml";
+            //openFileDialog1.ShowDialog();
+            DialogResult dr = this.openFileDialog1.ShowDialog();
+            try
+            {
+                if (dr == System.Windows.Forms.DialogResult.OK)
+                {
+                    path = openFileDialog1.FileName;
+                    dataSet.ReadXml(@"" + path);
+                    dataGridView1.DataSource = dataSet.Tables[0];
+                    dataGridView1.Refresh();
+                    int counts = dataGridView1.Columns.Count;
+                    CMBsearchcol.Items.Add("<----SELECT VALUE---->");
+                    for (int i = 0; i <counts; i++)
+                    {
+
+                        CMBsearchcol.Items.Add(dataGridView1.Columns[i].Name.ToUpper());
+                    }
+                    
+                    if (dataGridView1.Rows.Count == 0)
+                    {
+                        txtSearch.Enabled = false;
+                        BtnSearch.Enabled = false;
+                    }
+                    else
+                    {
+                        txtSearch.Enabled = true;
+                        BtnSearch.Enabled = true;
+                        CMBsearchcol.Enabled = true;
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("File Not Selected", "XML READER");
+                    txtSearch.Enabled = false;
+                    BtnSearch.Enabled = false;
+                    dataGridView1.DataSource = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Invalid FILE");
+            }
+        }
+
+        private void BtnSearch_Click(object sender, EventArgs e)
+        {
+
+            string search = txtSearch.Text;
+            searchItem = CMBsearchcol.SelectedItem.ToString();
+            
+            
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            int counts = dataGridView1.Columns.Count;
+            if (search == "")
+            {
+                MessageBox.Show("Enter Value To Search", "XML READER");
+            }
+            else
+            {
+                try
+                {
+                    foreach (DataGridViewRow row in dataGridView1.Rows)
+                    {
+
+
+                        if (row.Cells[searchItem].Value.ToString().Equals(search))
+                        {
+                            row.Selected = true;
+                            row.Visible = true;
+
+                        }
+                        else
+                        {
+                            CurrencyManager currencyManager1 = (CurrencyManager)BindingContext[dataGridView1.DataSource];
+                            currencyManager1.SuspendBinding();
+                            row.Selected = false;
+                            row.Visible = false;
+                            currencyManager1.ResumeBinding();
+                        }
+
+                    }
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(exc.Message, "XML READER");
+                }
+            }
+        
+        }
+
+        private void CMBsearchcol_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtSearch.Text = "";
+            
+            searchItem = CMBsearchcol.SelectedItem.ToString();
+            string item=(CMBsearchcol.SelectedItem.ToString()).Trim();
+            BtnReset.Enabled = true;
+            if (CMBsearchcol.SelectedIndex==0 && rst==0)
+            {
+                MessageBox.Show("Invalid Column Name", "XML READER");
+            }
+            else if (rst == 1)
+            {
+                rst = 0;
+            }
+            else
+            {
+            }
+            
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            if (CMBsearchcol.SelectedValue ==null && searchItem==null)
+            {
+                MessageBox.Show("Select a Column Name", "XML READER");
+                CMBsearchcol.Focus();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            txtSearch.Text = "";
+            rst = 1;
+            CMBsearchcol.SelectedIndex = 0;
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UTF8Encoding utf8 = new UTF8Encoding();
+            Byte[] encodedBytes = System.Text.Encoding.UTF8.GetBytes("\uD83D\uDDD6");
+            String decodedString = utf8.GetString(encodedBytes);
+            BTnMax.Text = decodedString;
+            BtnReset.Enabled = false;
+            BtnSearch.Enabled = false;
+            txtSearch.Enabled = false;
+            CMBsearchcol.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BTnMax_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void BTNABOUT_Click(object sender, EventArgs e)
+        {
+            Mozhi frm = new Mozhi();
+            frm.ShowDialog();
+        }
+
+        
+        
+    }
+}
